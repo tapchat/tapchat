ccd83647faac9e5381cdc5f9d05de389b5c67f1c
Eric Butler <eric@codebutler.com>
2013-08-23 20:33:45 -0700

    add missing migration file

---
+++ db/schema_5_up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    uid         INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name        TEXT     NOT NULL UNIQUE,
+    password    TEXT     NOT NULL,
+    is_admin    BOOLEAN  NOT NULL DEFAULT 0,
+    created_at  DATETIME DEFAULT (strftime('%s','now')),
+    updated_at  DATETIME
+);
+
+CREATE INDEX users_name ON users (name);
+
+ALTER TABLE connections ADD COLUMN uid INTEGER;
+ALTER TABLE buffers ADD COLUMN uid INTEGER;
+
+UPDATE connections SET uid = 1;
+UPDATE buffers SET uid = 1;
+
+--- ALTER TABLE connections ALTER COLUMN uid NOT NULL;
+--- ALTER TABLE buffers ALTER COLUMN uid NOT NULL;
